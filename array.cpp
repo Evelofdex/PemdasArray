9b83194230a9012180e186d8ade2a729e5e81a23
Evelofdex <evelofdex@gmail.com>
2026-04-07 17:22:11 +0700

    Deklarasi tanpa inisialisasi

--- array.cpp
+++ array.cpp
@@ -4,4 +4,7 @@ using namespace std;
 int main(){
      //Deklarasi array disertai inisialisasi
     int dataku[5] = {10, 20, 30, 40, 50};
+    
+     //Deklarasi tanpa inisialisasi
+    int datamu[5];
 }
