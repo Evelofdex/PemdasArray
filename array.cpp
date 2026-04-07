432ac1de406edadb131e3207606d7fe1f0edbc16
Evelofdex <evelofdex@gmail.com>
2026-04-07 17:22:39 +0700

    Cara akses

--- array.cpp
+++ array.cpp
@@ -4,7 +4,10 @@ using namespace std;
 int main(){
      //Deklarasi array disertai inisialisasi
     int dataku[5] = {10, 20, 30, 40, 50};
-    
+
      //Deklarasi tanpa inisialisasi
     int datamu[5];
+
+     //Cara akses
+    cout << "Isi data kedua: " << dataku[1] << endl;
 }
