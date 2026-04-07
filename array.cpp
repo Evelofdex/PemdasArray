8d1813caee2823b65fb927ea0d3cc7ecca28a4ac
Evelofdex <evelofdex@gmail.com>
2026-04-07 17:37:44 +0700

    input array menggunakan for

--- array.cpp
+++ array.cpp
@@ -33,7 +33,18 @@ int main(){
     cout << "data ketiga = " << datamu[2] << endl;
     cout << "data keempat = " << datamu[3] << endl;
     cout << "data kelima = " << datamu[4] << endl;
-    
+    cout << endl;
+
+    cout << "Input array menggunakan For" << endl;
+    //mengisi data melalui perulangan
+    for (int i = 0; i <=4; i++){
+        cout << "Isikan data ke-" << (i + 1) << " : ";
+        cin >> datamu[i];
+    }
+    cout << endl;
+
+    cout << "Tampil data array menggunakan for" << endl;
+    //menampilkan data dengan perulangan
 
 
     
